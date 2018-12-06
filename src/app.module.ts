13bfb5539b1e8b1b7b9337aa543b5ea804ac382a
Lucas Trindade <lucasktrindade@gmail.com>
2018-12-05 23:29:41 -0200

    :art: Adiciona dist no ignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { OrdersController } from './orders/orders.controller';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule, MongooseModule.forRoot('mongodb://spdif:spidf123@ds125684.mlab.com:25684/store-whitelabel')],
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb://test:test123@ds125684.mlab.com:25684/store-whitelabel')],
   controllers: [TypesController, OrdersController]
 })
 export class AppModule {}
